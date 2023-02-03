f269b09ffd8fd8cb6ed9fea520696f2acd4457e2
matekuna92 <kunamate66@gmail.com>
2023-02-03 22:29:12 +0100

    access store counter in Counter component, useSelector hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,18 @@
+import { useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  // get access to date managed by the store with this function
+  // react-redux automatically set up a subscription to the redux store for this component, no manual need
+  const counter = useSelector(state => state.counter);    // store the store's counter in a variable
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
