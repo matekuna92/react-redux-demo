eae8d263fee85bb7da7b2d15797e30b8edc0eb24
matekuna92 <kunamate66@gmail.com>
2023-02-04 10:52:13 +0100

    usedispatch, inc, dec handler, no state change

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
   // get access to date managed by the store with this function
   // react-redux automatically set up a subscription to the redux store for this component, no manual need
   const counter = useSelector(state => state.counter);    // store the store's counter in a variable
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment',});
+  }
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  }
 
   const toggleCounterHandler = () => {};
 
@@ -13,6 +22,8 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={decrementHandler}>Decrement</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
