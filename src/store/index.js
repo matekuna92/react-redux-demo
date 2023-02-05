a314dd2153aef770cd3c7271091235c31ff7d995
matekuna92 <kunamate66@gmail.com>
2023-02-05 22:29:39 +0100

    can use constant to avoid typo on action type

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore } from 'redux';
 
+export const INCREMENT = 'increment';
 const initialState = { counter: 0, showCounter: true };
 
 const counterReducer = (state = initialState, action) => {     // default 0 value for first run
-    if(action.type === 'increment') {
+    if(action.type === INCREMENT) {
         return {
             counter: state.counter + 1,
             showCounter: state.showCounter
